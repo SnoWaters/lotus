65eaab857e06f2da99987573bc90faf7f0d1fd08
SnoWaters <61969432+SnoWaters@users.noreply.github.com>
2020-03-09 11:31:17 +0100

    Add files via upload

---
+++ produits.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Lotus d'Asie</title>
+</head>
+<body>
+<div class="hh">
+	<header>Tel: 09 73 18 34 50 |E-MAIL:lotusdasie@live.fr</header>
+</div>
+<div class="menu">
+	<h1> <em>(Logo)</em>Lotus d'Asie</h1>
+	<nav>
+		<ul>
+			<li class="fixe">Acceuil </li>
+			<li class="fixe">Presentation produit</li>
+			<li class="fixe">Commande </li>
+			<li class="fixe"> Espace Communication </li>
+		</ul>
+	</nav>
+</div>
+<div class="corps">
+	
+</div>
+<div class="red"> </div>
+<div class="footer ">&copy 2019 Lotus d'Asie </div>
+</body>
+</html>
